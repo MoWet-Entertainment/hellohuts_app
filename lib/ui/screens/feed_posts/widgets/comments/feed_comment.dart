7c00c5d3ac053b53d9c65040c32944d48f4dbc10
Vinoop K S <vinoopks@hellohuts.com>
2020-09-26 22:26:46 +0530

    added share widget

--- lib/ui/screens/feed_posts/widgets/comments/feed_comment.dart
+++ lib/ui/screens/feed_posts/widgets/comments/feed_comment.dart
@@ -1,13 +1,15 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:hellohuts_app/constants/constants.dart';
 import 'package:hellohuts_app/constants/hello_icons.dart';
 import 'package:hellohuts_app/ui/routes/router.gr.dart';
 import 'package:hellohuts_app/ui/styles/app_colors.dart';
+import 'package:hellohuts_app/ui/styles/app_themes.dart';
 
 class CommentButton extends StatelessWidget {
   final String postId;
-  final String commentCount;
-  const CommentButton({
+   String commentCount;
+   CommentButton({
     Key key,
     this.postId,
     this.commentCount = '',
@@ -15,21 +17,18 @@ class CommentButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    this.commentCount = "12";
     return GestureDetector(
       child: Row(
         children: <Widget>[
-          Icon(
-            HelloIconsOld.comment,
-            size: 21,
-            color: AppColors.kAlmostBlack,
-          ),
+         Image.asset(HelloIcons.comment_bold_icon, color: AppColors.kDarkestGrey, height: 22,),
           SizedBox(
             width: 4.0,
           ),
-          // Text(
-          //   commentCount,
-          //   style: TextStyle(fontSize: 10),
-          // )
+          Text(
+            commentCount,
+            style: AppThemes.normalTextStyle,
+          )
         ],
       ),
       onTap: () {
