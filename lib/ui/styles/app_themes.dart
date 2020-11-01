e970e6c31a2e88311f3dd95d8fda369426495167
Vinoop K S <vinoopks@hellohuts.com>
2020-11-01 23:59:58 +0530

    ThemeData added

--- lib/ui/styles/app_themes.dart
+++ lib/ui/styles/app_themes.dart
@@ -6,7 +6,7 @@ import 'package:google_fonts/google_fonts.dart';
 class AppThemes {
   static BuildContext context;
 
-  static ThemeData themeData(bool isDarkTheme, BuildContext context) {
+  static ThemeData themeData1(bool isDarkTheme, BuildContext context) {
     return ThemeData(
       primaryColor: AppColors.kbPureWhite,
       indicatorColor: AppColors.kbPrimaryYellow,
@@ -30,13 +30,16 @@ class AppThemes {
             : AppColors.kbPrimaryColorLight,
         elevation: 0.0,
       ),
-      accentColor:
-          isDarkTheme ? AppColors.kbAccentColorDark : AppColors.kbAccentColorDark,
-      bottomAppBarColor:
-          isDarkTheme ? AppColors.kbAccentColorDark : AppColors.kbAccentColorDark,
+      accentColor: isDarkTheme
+          ? AppColors.kbAccentColorDark
+          : AppColors.kbAccentColorDark,
+      bottomAppBarColor: isDarkTheme
+          ? AppColors.kbAccentColorDark
+          : AppColors.kbAccentColorDark,
       bottomSheetTheme: Theme.of(context).bottomSheetTheme.copyWith(
-            modalBackgroundColor:
-                isDarkTheme ? AppColors.kbAccentColorDark : AppColors.kbPureWhite,
+            modalBackgroundColor: isDarkTheme
+                ? AppColors.kbAccentColorDark
+                : AppColors.kbPureWhite,
             modalElevation: 1,
           ),
       colorScheme: ColorScheme(
@@ -46,15 +49,18 @@ class AppThemes {
         brightness: isDarkTheme ? Brightness.dark : Brightness.light,
         error: isDarkTheme ? AppColors.kbDarkRed : AppColors.kbDarkRed,
         onError: isDarkTheme ? AppColors.kbPureWhite : AppColors.kbPureBlack,
-        surface: isDarkTheme ? AppColors.kbDark_4 : AppColors.kbPrimaryColorLight,
+        surface:
+            isDarkTheme ? AppColors.kbDark_4 : AppColors.kbPrimaryColorLight,
         primary: isDarkTheme
             ? AppColors.kbPrimaryColorDark
             : AppColors.kbPrimaryColorLight,
         onPrimary: isDarkTheme ? Colors.white : Colors.black,
-        primaryVariant: isDarkTheme ? AppColors.kbDark_1 : AppColors.kbAliceBlue,
+        primaryVariant:
+            isDarkTheme ? AppColors.kbDark_1 : AppColors.kbAliceBlue,
         secondary:
             isDarkTheme ? AppColors.kbDark_3 : AppColors.kbPrimaryColorLight,
-        secondaryVariant: isDarkTheme ? AppColors.kbDark_2 : AppColors.kbLavender,
+        secondaryVariant:
+            isDarkTheme ? AppColors.kbDark_2 : AppColors.kbLavender,
         onSecondary: isDarkTheme ? Colors.white : Colors.black,
         onBackground: isDarkTheme ? Colors.white : Colors.black,
         onSurface: isDarkTheme ? Colors.white : Colors.black,
@@ -105,8 +111,8 @@ class AppThemes {
 
   static final TextStyle postedAuthorTextMainHeadStyle = GoogleFonts.lato(
     fontSize: 12.0,
-    textStyle:
-        TextStyle(fontWeight: FontWeight.w400, color: AppColors.kbDarkTextColor),
+    textStyle: TextStyle(
+        fontWeight: FontWeight.w400, color: AppColors.kbDarkTextColor),
   );
 
   static final TextStyle postedAuthorTextSubHeadStyle = GoogleFonts.lato(
@@ -132,29 +138,22 @@ class AppThemes {
   );
   static final TextStyle normalSecondaryTextStyle = GoogleFonts.muli(
     fontSize: 12.0,
-    textStyle:
-        TextStyle(fontWeight: FontWeight.w400, color: AppColors.kbDarkTextColor),
+    textStyle: TextStyle(
+        fontWeight: FontWeight.w400, color: AppColors.kbDarkTextColor),
   );
 
-  static final TextStyle appBarDefaultText = GoogleFonts.openSans(
-      fontSize: 16,
-      textStyle: TextStyle(
-          fontWeight: FontWeight.bold));
-
   static final darkTheme = ThemeData(
-     bottomNavigationBarTheme: BottomNavigationBarThemeData(
-          backgroundColor: AppColors.kbNavbarColorDark,
-          elevation: 1,
-          selectedItemColor: AppColors.kbPrimaryYellow,
-          unselectedItemColor: AppColors.kbPureWhite,
-        ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: AppColors.kbNavbarColorDark,
+      elevation: 1,
+      selectedItemColor: AppColors.kbPrimaryYellow,
+      unselectedItemColor: AppColors.kbPureWhite,
+    ),
     appBarTheme: AppBarTheme(
         textTheme: TextTheme(
-          headline6: appBarDefaultText.copyWith(color: AppColors.kbPrimaryYellow)
-        )
-      ),
-      textTheme: _normalTextTheme,
-      
+            headline6:
+                _appBarDefaultText.copyWith(color: AppColors.kbPrimaryYellow))),
+    textTheme: _normalTextTheme,
     colorScheme: ColorScheme(
       background: AppColors.kbNavbarColorDark,
       brightness: Brightness.dark,
@@ -173,18 +172,17 @@ class AppThemes {
   );
 
   static final lightTheme = ThemeData(
-        bottomNavigationBarTheme: BottomNavigationBarThemeData(
-          backgroundColor: AppColors.kbNavbarColorLight,
-          elevation: 1,
-          selectedItemColor: AppColors.kbPrimaryYellow,
-          unselectedItemColor: AppColors.kbPureWhite,
-        ),
-      appBarTheme: AppBarTheme(
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: AppColors.kbNavbarColorLight,
+      elevation: 1,
+      selectedItemColor: AppColors.kbPrimaryYellow,
+      unselectedItemColor: AppColors.kbPureWhite,
+    ),
+    appBarTheme: AppBarTheme(
         textTheme: TextTheme(
-          headline6: appBarDefaultText.copyWith(color: AppColors.kbPureBlack)
-        )
-      ),
-      textTheme: _normalTextTheme,
+            headline6:
+                _appBarDefaultText.copyWith(color: AppColors.kbPureBlack))),
+    textTheme: _normalTextTheme,
     colorScheme: ColorScheme(
       background: AppColors.kbPrimaryColorLight,
       brightness: Brightness.light,
@@ -202,8 +200,7 @@ class AppThemes {
     ),
   );
 
-
-          /// Creates a [MaterialColor] based on the supplied [Color]
+  /// Creates a [MaterialColor] based on the supplied [Color]
   static MaterialColor createMaterialColor(Color color) {
     List strengths = <double>[.05];
     Map swatch = <int, Color>{};
@@ -224,20 +221,112 @@ class AppThemes {
     return MaterialColor(color.value, swatch);
   }
 
+  static ThemeData lightThemedata =
+      themeData(lightColorScheme, _appBarThemeLight, AppColors.kLightFocusColor);
+
+  static ThemeData darkThemeData =
+      themeData(darkColorScheme, _appBarThemeDark, AppColors.kDarkFocusColor);
+
+  static ThemeData themeData(
+      ColorScheme colorScheme, AppBarTheme appbarTheme, Color focusColor) {
+    return ThemeData(
+      colorScheme: colorScheme,
+      textTheme: _textTheme,
+      primaryColor: colorScheme.primary,
+      appBarTheme: appbarTheme.copyWith(color: colorScheme.background, brightness: colorScheme.brightness),
+      iconTheme: IconThemeData(color: colorScheme.onPrimary),
+      canvasColor: colorScheme.background,
+      scaffoldBackgroundColor: colorScheme.background,
+      highlightColor: Colors.transparent,
+      accentColor: colorScheme.primary,
+      focusColor: focusColor,
+
+    );
+  }
+
+  static final TextStyle _appBarDefaultText = GoogleFonts.openSans(
+      fontSize: 16, textStyle: TextStyle(fontWeight: FontWeight.bold));
+
+  static AppBarTheme _appBarThemeLight = AppBarTheme(
+    elevation: 0,
+      textTheme: TextTheme(
+          headline6:
+              _appBarDefaultText.copyWith(color: AppColors.kbPureBlack)));
+  static AppBarTheme _appBarThemeDark = AppBarTheme(
+      textTheme: TextTheme(
+          headline6:
+              _appBarDefaultText.copyWith(color: AppColors.kbPrimaryYellow)));
+
+
+  static const _light = FontWeight.w300;
+  static const _regular = FontWeight.w400;
+  static const _medium = FontWeight.w500;
+  static const _semiBold = FontWeight.w600;
+  static const _bold = FontWeight.w700;
+  static final TextTheme _textTheme = TextTheme(
+    headline1: GoogleFonts.lato(fontWeight: _bold, fontSize: 32),
+    headline2: GoogleFonts.lato(fontWeight: _bold, fontSize: 24),
+    headline3: GoogleFonts.lato(
+      fontWeight: _bold,
+      fontSize: 20,
+    ),
+    headline4: GoogleFonts.muli(fontWeight: _bold, fontSize: 16),
+    headline5: GoogleFonts.muli(fontWeight: _bold, fontSize: 16),
+    headline6: GoogleFonts.lato(
+      fontWeight: _bold,
+      fontSize: 14,
+    ),
+    subtitle1: GoogleFonts.lato(fontWeight: _medium, fontSize: 14),
+    subtitle2: GoogleFonts.muli(fontWeight: _medium, fontSize: 14),
+    bodyText1: GoogleFonts.lato(fontWeight: _regular, fontSize: 14),
+    bodyText2: GoogleFonts.muli(fontWeight: _regular, fontSize: 14),
+    overline: GoogleFonts.lato(fontWeight: _regular, fontSize: 12),
+    caption: GoogleFonts.muli(fontWeight: _regular, fontSize: 12),
+    button: GoogleFonts.lato(fontWeight: _bold, fontSize: 14),
+  );
+
+  static const ColorScheme lightColorScheme = ColorScheme(
+      primary: AppColors.kPrimaryLight,
+      primaryVariant: AppColors.kPrimaryVariantLight,
+      secondary: AppColors.kSecondaryLight,
+      secondaryVariant: AppColors.kSecondaryVariantLight,
+      background: AppColors.kPureWhite,
+      onBackground: AppColors.kPureBlack,
+      error: AppColors.kRed,
+      onError: AppColors.kPureWhite,
+      onPrimary: AppColors.kPureWhite,
+      onSecondary: AppColors.kPureBlack,
+      surface: AppColors.kbSmokedWhite,
+      onSurface: AppColors.kPureBlack,
+      brightness: Brightness.light);
+
+  static const ColorScheme darkColorScheme = ColorScheme(
+      primary: AppColors.kPrimaryDark,
+      primaryVariant: AppColors.kPrimaryVariantDark,
+      secondary: AppColors.kSecondaryDark,
+      secondaryVariant: AppColors.kSecondaryVariantDark,
+      background: AppColors.kDark_1,
+      onBackground: AppColors.kPureWhite,
+      error: AppColors.kRed,
+      onError: AppColors.kPureWhite,
+      onPrimary: AppColors.kPureWhite,
+      onSecondary: AppColors.kPureWhite,
+      surface: AppColors.kDark_10,
+      onSurface: AppColors.kPureWhite,
+      brightness: Brightness.dark);
 }
 
-  final TextTheme _normalTextTheme = TextTheme(
-        headline6: GoogleFonts.lato(
-          fontSize: 12,
-        ),
-        caption: GoogleFonts.lato(
-          fontSize:12,
-        ),
-        bodyText1: GoogleFonts.lato(
-          fontSize: 14,
-        ),
-        bodyText2: GoogleFonts.muli(
-          fontSize: 14,
-        ),
-        
-        );
+final TextTheme _normalTextTheme = TextTheme(
+  headline6: GoogleFonts.lato(
+    fontSize: 12,
+  ),
+  caption: GoogleFonts.lato(
+    fontSize: 12,
+  ),
+  bodyText1: GoogleFonts.lato(
+    fontSize: 14,
+  ),
+  bodyText2: GoogleFonts.muli(
+    fontSize: 14,
+  ),
+);
